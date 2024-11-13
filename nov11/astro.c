8318d4cee579cb16e7fa3417c5ddc7ff5ff510a2
Keerthis1505 <keerthinaidu1505@gmail.com>
2024-11-13 13:23:39 +0530

    Add files via upload

---
+++ nov11/astro.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char name[30];
+    int countOfChar=0,is_even=0;
+    printf("enter your name");
+    scanf("%c",&name);
+    // printf("your name: %c");
+    if(name==%2){
+        printf("it is an even number");{
+            name==toupper;
+        }
+    }
+    else {
+        printf("it is an odd number");{
+            name==tolower;
+        }
+    }
+}
